0efdd7f7fbaad7fd18fd9fa0be1652c856f8e2ce
JagrutiKate47 <127717124+JagrutiKate47@users.noreply.github.com>
2026-06-11 16:22:59 +0530

    Update main.cpp

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,1 +1,28 @@
+#include <Arduino.h>
 
+#define VOLTAGE_PIN 36
+#define CURRENT_PIN 39
+
+void setup()
+{
+    Serial.begin(115200);
+}
+
+void loop()
+{
+    int voltageValue = analogRead(VOLTAGE_PIN);
+    int currentValue = analogRead(CURRENT_PIN);
+
+    Serial.print("Voltage ADC: ");
+    Serial.print(voltageValue);
+
+    Serial.print(" Current ADC: ");
+    Serial.println(currentValue);
+
+    if(voltageValue < 500)
+    {
+        Serial.println("Voltage Fault Detected");
+    }
+
+    delay(1000);
+}
